e21727cbd38bcb4b47f26850b0fbc3641ba3cbc6
Alan Voigt <alanvoigt@yahoo.com.br>
2024-02-07 07:10:40 -0300

    First commit

---
+++ test.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  let response = http.get('http://test.k6.io');
+  check(response, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
