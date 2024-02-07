c05cbc235af13464326a2b046d0f5e933d7ea4c2
Alan Voigt <alanvoigt@yahoo.com.br>
2024-02-07 07:15:27 -0300

    second commit

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+
+export let options = {
+    vus: 1, // 1 usuário virtual
+    duration: '10s', // Executar por 10 segundos
+};
+
 export default function () {
   let response = http.get('http://test.k6.io');
   check(response, { 'status was 200': (r) => r.status == 200 });
